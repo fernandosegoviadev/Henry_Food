7f37e820cfdd480cfa9e597ca3816686181902cd
nano0144 <fer_0144@hotmail.com>
2022-05-16 19:41:14 -0300

    Henry Food 2022

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,6 +2,6 @@ import './Footer.css'
 
 export default function Footer () {
     return (
-        <div id = 'pie'>Henry Food 2022</div>
+        <div id = 'pie'>Henry Food 2022 - https://github.com/nano0144</div>
     )
 }
